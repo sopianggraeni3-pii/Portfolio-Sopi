8f687ffd5f424de1e9d399aa9673df23e7602f78
sopi <24700611121@student.unsil.ac.id>
2026-02-13 21:14:07 +0700

    Upload portofolio pertama

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function showMessage() {
+    alert("Terima kasih telah mengunjungi portofolio saya!");
+}
+
+var typed= new typed(".text", {
+    String:["Web Developer", "Ilustrator", "crocheter"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
